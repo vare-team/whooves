ee7e6365acd50699ed70c3ee2bc500b8d9681fbb
Василий <vasiliy072000mail.ru@gmail.com>
2019-07-15 13:51:58 +0300

    Update Akin.js

--- Akin.js
+++ Akin.js
@@ -1,58 +1,63 @@
-console.log('Инициализация ядра...\nПодключение библиотек...');
-var colors = require('colors');
-const Discord = require('discord.js');
-  client = new Discord.Client({disableEveryone: true});
-console.log(`discord.js`.black.bgWhite + ` библиотека успешно подключена!`.white);
-const Enmap = require("enmap");
-console.log(`enmap`.black.bgWhite + ` библиотека успешно подключена!`.white);
+let color = require('colors');
+const Discord = require('discord.js'),
+client = new Discord.Client();
 const fs = require("fs");
-console.log(`fs`.black.bgWhite + ` библиотека успешно подключена!`.white);
-var mysql = require('mysql'),
-  mysqlUtilities = require('mysql-utilities');
-console.log(`mysql`.black.bgWhite + ` библиотека успешно подключена!`.white);
 
-var client.db = mysql.createConnection({user: '', password: '', database: 'Akin', charset: "utf8mb4"});
-client.db.on('error', (err) => {console.log(err)});
-client.db.connect((err) => {if (err) return console.error('error connecting: ' + err.stack); console.log('Соединение с бд '.white+ `${client.db.threadId}`.black.bgWhite + ` успешно!`.white); delete err;});
-mysqlUtilities.upgrade(client.db);
-mysqlUtilities.introspection(client.db);
+let con = require('mysql').createConnection({user: "", password: "", database: "Akin", charset: "utf8mb4"});
+con.on('error', (err) => {console.warn(err)});
+con.connect(() => {client.userLib.sendlog(`{DB Connected} (ID:${con.threadId})`);});
+require('mysql-utilities').upgrade(con);
+require('mysql-utilities').introspection(con);
 
-console.log(`Подключение конфига `.white+` config.json`.black.bgWhite);
-client.config = require("./config");
-client.discord = Discord;
-console.log(`Конфиг `.white+`config.json`.black.bgWhite+` загржен!\n`.white);
+client.userLib = {};
 
-fs.readdir("/root/bots/akin/events/", (err, files) => {
+client.userLib.config = require("./config");
+client.userLib.discord = Discord;
+client.userLib.db = con;
+client.userLib.presenseCount = 0;
+// client.userLib.cooldown = new Map();
+
+client.userLib.sendlog = (log) => {
+  const now = new Date();
+  console.log(`${('00' + now.getHours()).slice(-2) + ':' + ('00' + now.getMinutes()).slice(-2) + ':' + ('00' + now.getSeconds()).slice(-2)} | Shard[${client.shard.id}] : ${log}`);
+};
+
+client.userLib.presenseFunc = () => {
+  switch (client.userLib.presenseCount) {
+    case 1:
+      client.user.setPresence({ game: { name: `Серверов на ${client.userLib.shardName[client.shard.id]}: ${client.guilds.size}`, type: 'LISTENING' }});
+      client.userLib.presenseCount = 0;
+      break;
+  }
+
+  client.userLib.presenseCount++;
+};
+
+con.queryKeyValue('SELECT id, tier FROM admins WHERE 1', (err, result) => client.userLib.admins = result);
+
+fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
-  let counter = files.length;
-  let counteris = 0;
-  files.forEach(file => {
-    counteris++;
-    if (!file.endsWith(".js")) return;
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    console.log(`Ивент `.white+`${eventName}`.black.bgWhite+` загржен. [${counteris}/${counter}]`.white);
-    delete require.cache[require.resolve(`./events/${file}`)];
+  files.filter(file => file.endsWith('.js')).forEach(file => {
+    try {
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      client.on(eventName, event.bind(null, client));
+      delete require.cache[require.resolve(`./events/${file}`)];
+    } catch (e) {console.warn(e)}
   });
-  if (counter == counteris) console.log('Все ивенты загружены!\n'.white);
 });
 
-client.commands = new Enmap();
+client.commands = new Discord.Collection();
 
-fs.readdir("/root/bots/akin/commands/", (err, files) => {
+fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
-  let counter = files.length;
-  let counteris = 0;
-  files.forEach(file => {
-    counteris++;
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-    console.log(`Команда `.white+`${commandName}`.black.bgWhite+` загржена. [${counteris}/${counter}]`.white);
+  files.filter(file => file.endsWith('.js')).forEach(file => {
+    try {
+      let props = require(`./commands/${file}`);
+      let commandName = file.split(".")[0];
+      client.commands.set(commandName, props);
+    } catch (e) {console.warn(e)}
   });
-  if (counter == counteris) console.log('Все команды загружены!\n'.white);
 });
 
-client.login(client.config.token);
+client.login(client.userLib.config.token);
