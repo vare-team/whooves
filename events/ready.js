71059617d5053d72949fe8c4ff63d56bdda92c3a
Boris <chatsky.inc@yandex.ru>
2019-07-24 02:12:23 +0800

    update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-  client.user.setPresence({ game: { name: client.config.game.replace("{prefix}", client.config.prefix), type: client.config.gtype }});
-  console.log(`Бот авторизован как `.white + `${client.user.tag}`.black.bgWhite + `!`.white);
+  client.user.setPresence({ game: { name: client.userLib.config.game.replace("{prefix}", client.userLib.config.prefix), type: client.userLib.config.gtype }});
+  console.log(`Бот авторизован как ${client.user.tag}`);
 };
