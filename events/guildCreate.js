84c0240636a60e6611aeb3bb870d35eef4850465
Boris <chatsky.inc@yandex.ru>
2019-08-01 02:10:50 +0800

    fix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,5 @@
 module.exports = (client, guild) => {
   client.userLib.db.insert(`guilds`, {id: guild.id, moneyico: '💸'}, () => {});
   // client.userLib.db.insert(`guilds`, {id: guild.id}, () => {});
-  client.userLib.sendLog(`Новый сервер "${guild.name}", владелец "${guild.owner.user.tag}", всего там "${guild.memberCount}" участников.`);
+  client.userLib.sendlog(`Новый сервер "${guild.name}", владелец "${guild.owner.user.tag}", всего там "${guild.memberCount}" участников.`);
 };
--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ talkedMoney = new Set();
 talkedcool = new Set();
 
 function isUrl(s) {
-    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    let regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
     return regexp.test(s);
 }
 
