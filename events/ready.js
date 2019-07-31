a927cf310a8f2f6b0a2edee4cb805f852cc32327
Boris <chatsky.inc@yandex.ru>
2019-08-01 01:35:00 +0800

    delete presense

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,3 @@
 module.exports = (client) => {
-  client.user.setPresence({ game: { name: client.userLib.config.game.replace("{prefix}", client.userLib.config.prefix), type: client.userLib.config.gtype }});
   console.log(`Бот авторизован как ${client.user.tag}`);
 };
