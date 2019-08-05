e70144c430a2d14495070e897ab2ea2f1bb33d9e
Boris <chatsky.inc@yandex.ru>
2019-08-05 19:34:07 +0800

    Update events (logs)

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,12 +1,7 @@
 module.exports = (client, member) => {
-
-  client.userLib.db.queryValue('SELECT logchannel FROM guilds WHERE id = ?', [member.guild.id], (err, logchannel) => {
-    if (logchannel == '0') return;
-    let av = member.user.avatarURL;
-    if (!member.user.avatarURL) av = member.user.defaultAvatarURL;
-
-    let sendlogchannel = client.channels.get(logchannel);
-    if (!sendlogchannel) return client.userLib.db.upsert(`servers`, {id: member.guild.id, logchannel: 0}, (err) => {});
-    sendlogchannel.send({embed}).catch(err => console.log(`\nОшибка!\nСервер: ${member.guild.name} (ID: ${member.guild.id})\nПользователь: ${member.user.tag} (ID: ${member.user.id})\nТекст ошибки: ${err}`));
-  }); 
+    client.userLib.sendLogChannel("memberRemove", member.guild, {user: {
+      tag: member.user.tag,
+      id: member.id,
+      avatar: member.user.displayAvatarURL
+    }});
 };
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,36 +1,38 @@
 let embed;
 
 module.exports = async (client, oldMember, newMember) => {
-  client.userLib.db.queryValue('SELECT logchannel FROM guilds WHERE id = ?', [oldMember.guild.id], (err, logchannel) => {
-    if (logchannel == '0') return;
-    let sendlogchannel = client.channels.get(logchannel);
-    if (!newMember.user.avatarURL) newMember.user.avatarURL = newMember.user.defaultAvatarURL;
-    if (!sendlogchannel) return client.db.upsert(`guilds`, {id: msg.guild.id, logchannel: 0}, (err) => {});
-
     if (!oldMember.voiceChannel) {
-      embed = new client.userLib.discord.RichEmbed()
-      .setColor(client.userLib.config.colors.inf)
-      .setTitle(`Подключился к "${newMember.voiceChannel.name}"`)
-      .setAuthor(oldMember.user.tag, newMember.user.avatarURL)
-      .setFooter(`ID: ${oldMember.user.id}`)
-      .setTimestamp();
+
+      client.userLib.sendLogChannel("voiceStateAdd", newMember.guild, {user: {
+        tag: oldMember.user.tag,
+        id: oldMember.id,
+        avatar: oldMember.user.displayAvatarURL,
+        voice: newMember.voiceChannel
+      }});
+
     };
     if (!newMember.voiceChannel) {
-      embed = new client.userLib.discord.RichEmbed()
-      .setColor(client.userLib.config.colors.inf)
-      .setTitle(`Отключился от "${oldMember.voiceChannel.name}"`)
-      .setAuthor(oldMember.user.tag, newMember.user.avatarURL)
-      .setFooter(`ID: ${oldMember.user.id}`)
-      .setTimestamp();
+
+      client.userLib.sendLogChannel("voiceStateRemove", oldMember.guild, {user: {
+        tag: oldMember.user.tag,
+        id: oldMember.id,
+        avatar: oldMember.user.displayAvatarURL,
+        voice: oldMember.voiceChannel
+      }});
+
     };
+
     if (oldMember.voiceChannel && newMember.voiceChannel) {
-      embed = new client.userLib.discord.RichEmbed()
-      .setColor(client.userLib.config.colors.inf)
-      .setTitle(`Переместился из "${oldMember.voiceChannel.name}" в "${newMember.voiceChannel.name}"`)
-      .setAuthor(oldMember.user.tag, newMember.user.avatarURL)
-      .setFooter(`ID: ${oldMember.user.id}`)
-      .setTimestamp();
+
+      client.userLib.sendLogChannel("voiceStateUpdate", oldMember.guild, {user: {
+        tag: oldMember.user.tag,
+        id: oldMember.id,
+        avatar: oldMember.user.displayAvatarURL,
+        voice: {
+          oldName: oldMember.voiceChannel.name,
+          newName: newMember.voiceChannel.name
+        }
+      }});
       
     }
-  });
 };
