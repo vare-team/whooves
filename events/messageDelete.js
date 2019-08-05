ab8bd12109ac1913b1cb8436735b21b35bee70fe
Boris <chatsky.inc@yandex.ru>
2019-08-05 23:43:02 +0800

    update message events (logs)

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,27 +6,15 @@ function isUrl(s) {
 }
 
 module.exports = (client, msg) => {
-
   if (msg.author.id == client.user.id) return;
   if (msg.author.bot) return;
 
-  client.userLib.db.queryValue('SELECT logchannel FROM guilds WHERE id = ?', [msg.guild.id], (err, logchannel) => {
-    if (msg.content == '') msg.content = 'Что-то';
-    if (logchannel == '0') return;
-    let av = msg.author.avatarURL;
-    if (!msg.author.avatarURL) av = msg.author.defaultAvatarURL;
-    embed = new Discord.RichEmbed()
-        .setColor(client.config.colors.inf)
-        .setTitle('Удалённое сообщение')
-        .setAuthor(msg.author.tag, av)
-        .setDescription(`\`\`\`${msg.content.replace(new RegExp("\`",'g'),"")}\`\`\``)
-        if (isUrl(msg.content)) embed.addField('Причина', 'Найдена ссылка.')
-        embed.setTimestamp()
-        .addField('Канал', `<#${msg.channel.id}>`)
-        .setFooter(`ID: ${msg.author.id}`);
-    let sendlogchannel = client.channels.get(logchannel);
-    if (!sendlogchannel) return client.userLib.db.upsert(`servers`, {id: msg.guild.id, logchannel: 0}, (err) => {});
-    sendlogchannel.send({embed}).catch(err => console.log(`\nОшибка!\nСервер: ${msg.guild.name} (ID: ${msg.guild.id})\nКанал: ${msg.channel.name} (ID: ${msg.channel.id})\nПользователь: ${msg.author.tag} (ID: ${msg.author.id})\nТекст ошибки: ${err}`));
-  });
-
+  if (msg.content == '') msg.content = 'Что-то';
+	client.userLib.sendLogChannel("messageDelete", msg.guild, {user: {
+		tag: msg.author.tag,
+		id: msg.author.id,
+    avatar: msg.author.displayAvatarURL,
+    content: msg.content,
+		channel: msg.channel
+	}});
 };
