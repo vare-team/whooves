a173f2c49a75c39191c60eaace69433da5984fb0
Vlad Ciphersky <32297717+vladciphersky@users.noreply.github.com>
2019-08-07 10:15:19 +0500

    oops, small fix

--- commands/say.js
+++ commands/say.js
@@ -8,11 +8,9 @@ exports.help = {
 
 exports.run = (client, msg, args, Discord) => {
 	var embed = new Discord.RichEmbed();
-	client.db.queryValue('SELECT prefix FROM guilds WHERE id = ?', [msg.guild.id], (err, prefix) => {
-		var text = args.join(" ");
-		if(!text) { embed.setColor(client.userLib.config.colors.err).setTitle('Ошибка!').setDescription(`Вы не ввели текст!`); return msg.channel.send(embed); }
+	var text = args.join(" ");
+	if(!text) { embed.setColor(client.userLib.config.colors.err).setTitle('Ошибка!').setDescription(`Вы не ввели текст!`); return msg.channel.send(embed); }
 
-		embed.setColor(client.userLib.config.colors.inf).setDescription(text);
-		return msg.channel.send(embed);
-	});
+	embed.setColor(client.userLib.config.colors.inf).setDescription(text);
+	return msg.channel.send(embed);
 }; 
