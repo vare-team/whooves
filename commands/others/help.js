5ef527d45a0bb53204afeaba10ba81c7ad4fbac5
Boris <chatsky.inc@yandex.ru>
2019-08-27 00:31:48 +0800

    update help command

--- commands/others/help.js
+++ commands/others/help.js
@@ -1,86 +1,56 @@
-let embed;
-const fs = require("fs");
-
 exports.help = {
-    name: "help",
-    description: "Вывести список всех команд, или вывести информацию об отдельной команде.",
-    usage: "help (команда)",
-    flag: 3,
-    cooldown: 500
+    name: 'help',
+    description: 'Лист команд, позволяет узнать более подробную информацию о каждой команде.',
+    aliases: ['commands'],
+    usage: '[command name]',
+    tier: 0,
+    cooldown: 5
 }
 
-exports.run = (client, msg, args, Discord) => {
+exports.run = async (client, msg, args) => {
+    const data = [];
+    const { commands } = msg.client;
 
-    client.userLib.db.queryValue('SELECT prefix FROM guilds WHERE id = ?', [msg.guild.id], (err, prefixtemp) => {
+    function list(cat, cname) {
+        return `**__${cname}__**: ${commands
+            .filter(cmd => cmd.module == cat)
+            .map(cmd => `\`${cmd.help.name}\``)
+            .join(", ")}`;
+    }
 
-        if (!args[1]) {
-
-    	   fs.readdir("./commands/", (err, files) => {
-                if(err) console.error(err);
-        
-                let jsfiles = files.filter(f => f.split(".").pop() === "js");
-                if(jsfiles.length <= 0) {
-                    console.log("No commands to load!");
-                    return;
-                }
-        
-                let gNamelistO = "";
-                let gNamelistA = "";
-                let gNamelistU = "";
-        
-                let flag = 3;
-        
-                if (msg.guild.ownerID == msg.author.id) flag = 1;
-                else if (msg.member.hasPermission('ADMINISTRATOR')) flag = 2;
-        
-                let result = jsfiles.forEach((f, i) => {
-                    let props = require(`./${f}`);
-                    let namelist = props.help.name;
-                    let desclist = props.help.description;
-                    let usage = props.help.usage;
-        
-                    if (props.help.flag == 1) gNamelistO += "\`\`" + prefixtemp + namelist + "\`\`, ";
-                    else if (props.help.flag == 2) gNamelistA += "\`\`" + prefixtemp + namelist + "\`\`, ";
-                    else if (props.help.flag == 3) gNamelistU += "\`\`" + prefixtemp + namelist + "\`\`, ";
-                });
-        
-                embed = new client.userLib.discord.RichEmbed()
-                    .setColor(client.userLib.colors.inf)
-                    .setDescription(`Напиши **${prefixtemp}help (команда)** для получения информации об определенной команде.\n\`\`[] - обязательный аргумент\n() - дополнительный аргумент\`\``)
-                    .setAuthor('Список команд AKin', client.user.avatarURL);
-        
-                switch (flag) {
-                    case 1:
-                        embed.addField('❯ Владелец', gNamelistO.substring(0, gNamelistO.length - 2));
-                    case 2:
-                        embed.addField('❯ Админ', gNamelistA.substring(0, gNamelistA.length - 2));
-                    case 3:
-                        embed.addField('❯ Пользователь', gNamelistU.substring(0, gNamelistU.length - 2));
-                        break;
-                }
-        
-                msg.channel.send({embed});
-            });
-        } else {
-
-            if (!client.commands.has(args[1])) return;
-
-            let props = require(`./${args[1]}.js`);
-            let namelist = props.help.name;
-            let desclist = props.help.description;
-            let usage = props.help.usage;
-            let cooldown = props.help.cooldown;
-
-            if (props.help.flag == 0) return;
-
-            embed = new client.userLib.discord.RichEmbed()
-            .setThumbnail(client.user.avatarURL)
-            .setColor(client.userLib.colors.inf)
-            .setTitle(`Команда ${prefixtemp}${namelist}`)
-            .addField('Описание', desclist)
-            .addField('Откат', `**${cooldown/1000}** сек.`)
-            .addField('Использование', `\`\`${prefixtemp}${usage}\`\``);
-            msg.channel.send({embed});
+    if (!args.length) {
+        data.push('Категории:');
+        if (client.userLib.admins.indexOf != -1) {
+            data.push(list('dev', 'Команды разработчиков:'));
         }
-    });
+        data.push(list('mod', 'Модерация и конфигурация:'));
+        data.push(list('social', 'Социальные:'));
+        data.push(list('games', 'Игры:'));
+        data.push(list('fun', 'fun:'));
+        data.push(list('others', 'Остальные:'));
+        data.push(`\nВы можете написать \`${prefix}help [command name]\` чтобы получить подробную информацию!`);
+
+        let embed = new client.userLib.discord.RichEmbed()
+            .setColor(client.userLib.colors.inf)
+            .setDescription(data)
+            .setTitle('Список команд');
+        return msg.channel.send({ embed: embed, split: true }).catch(err => {
+            msg.channel.send(data, { split: true })
+        });
+    }
+
+
+    const name = args[0].toLowerCase();
+    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+    if (!command) return msg.reply('Вы не указали команду!');
+
+    let embed = new client.userLib.discord.RichEmbed().setAuthor(client.user.username, client.user.avatarURL).setColor(client.userLib.colors.inf).setTitle("Команда: " + command.help.name);
+
+    if (command.help.aliases) embed.addField("Псевдонимы", command.help.aliases.join(', '));
+    if (command.help.description) embed.addField("Описание", command.help.description);
+    if (command.help.usage) embed.addField("Использование", `${prefix}${command.help.name} ${command.help.usage}`);
+    embed.addField("Временное ограничение", `${command.cooldown || 3} секунд`);
+
+    msg.channel.send(embed);
 };
