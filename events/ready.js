d1f34b2cc40d6bb94c092e44d17234369a20665a
MegaVasiliy007 <vasiliy072000mail.ru@gmail.com>
2020-01-11 01:24:13 +0300

    send sdc statistic

--- events/ready.js
+++ events/ready.js
@@ -5,12 +5,11 @@ module.exports = (client) => {
 	client.userLib.sc.registerTask({code: 'unMute', execute: (mutedRole, member) => {member.removeRole(mutedRole, 'Снятие мута!').catch(() => {});}});
 
 	client.userLib.presenceFunc();
-	// client.userLib.sendSDC();
+	if (process.env.sdc) client.userLib.sendSDC();
 
 	client.userLib.db.query('SELECT guildId, userId, time, mutedRole FROM mutes LEFT JOIN guilds using(guildId) WHERE time > now()', (err, fields) => {
-		for (let field of fields) {
+		for (let field of fields)
 			client.userLib.sc.pushTask({code: 'unMute', params: [field.mutedRole, client.guilds.get(field.guildId).members.get(field.userId)], time: field.time, timeAbsolute: true});
-		}
 	});
 
 	client.userLib.sendLog(`Shard ready!`, 'ShardingManager');
