d331df60f35bce5d33bc303e3955ce811853291a
MegaVasiliy007 <vasiliy072000mail.ru@gmail.com>
2020-01-11 01:23:50 +0300

    done pony commands

--- commands/pony/epony.js
+++ commands/pony/epony.js
@@ -1,23 +1,27 @@
 exports.help = {
-  name: "pony",
+  name: "epony",
   description: "Поиск по derpibooru.org\n**Для снятие цензуры, команду необходимо использовать в NSFW канале!**",
-  aliases: ['derpi', 'ps'],
-  usage: "(запрос) / >(ID)",
+  aliases: ['ederpi', 'eps'],
+  usage: "(запрос)",
   dm: 1,
   tier: 0,
   cooldown: 5
 };
 
 exports.run = async (client, msg, args) => {
-  let embedErr = new client.userLib.discord.RichEmbed().setTitle('Derpibooru IMAGE 404').setDescription("По вашему запросу ничего не найдено.").setImage('https://derpicdn.net/img/view/2019/12/29/2233270.gif').setFooter(msg.author.tag, msg.author.displayAvatarURL).setColor(client.userLib.colors.war);
+  if (!msg.channel.nsfw) {
+    client.userLib.retError(msg.channel, msg.author, 'Можно использовать только в NSFW канале!');
+    return;
+  }
 
-  let imageId = args[0] && args[0].startsWith('>') ?
-    args[0].replace(/\D+/g, '') :
-    (await client.userLib.request({url: `https://derpi.vlos.ru/search.json?random_image=true&filter_id=56027&q=${args[0] ? args[0] : '*'}`, json: true})).id;
-  if (!imageId) {msg.channel.send(embedErr);return;}
+  let imageId = await client.userLib.request({url: `https://derpi.vlos.ru/search.json?random_image=true&filter_id=175480&q=explicit${args[0] ? ',' + args[0] : ''}`, json: true});
+  imageId = imageId.id;
+  if (!imageId) {
+    let embedErr = new client.userLib.discord.RichEmbed().setTitle('Derpibooru IMAGE 404').setDescription("По вашему запросу ничего не найдено.").setImage('https://derpicdn.net/img/view/2019/12/29/2233270.gif').setFooter(msg.author.tag, msg.author.displayAvatarURL).setColor(client.userLib.colors.war);
+    msg.channel.send(embedErr);return;
+  }
 
-  let pony = await client.userLib.request({url: `https://derpi.vlos.ru/api/v1/json/images/${imageId}`, json: true}).catch(e => 0);
-  if (!pony) {msg.channel.send(embedErr);return;}
+  let pony = await client.userLib.request({url: `https://derpi.vlos.ru/api/v1/json/images/${imageId}`, json: true});
   pony = pony.image;
 
   let description = pony.tags.reduce((pr, cu) => pr += cu + ',', "**Тэги:** ").slice(0, -1);
@@ -30,6 +34,5 @@ exports.run = async (client, msg, args) => {
     .addField('Комментарии',`:pencil:**${pony.comment_count}**`, true)
     .setColor(client.userLib.colors.inf)
     .setURL('https://derpibooru.org/images/' + pony.id);
-  if (!msg.channel.nsfw && pony.tags.indexOf("explicit") != -1) embed.setImage("https://derpicdn.net/media/2014/01/07/21_08_05_460_rect4.png");
   msg.channel.send(pony.tags.indexOf("futa") != -1 ? '<@264773177815728129>' : '', embed);
 };
