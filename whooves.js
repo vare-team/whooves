a1c19e3eecbf4f588e38970f49b5402e77a9f2d1
Максим <37771198+MasterJ0y@users.noreply.github.com>
2021-05-30 17:48:22 +0300

    Interactions

--- whooves.js
+++ whooves.js
@@ -21,9 +21,31 @@ readdir("./events/", (err, files) => {
       const event = require(`./events/${file}`);
       client.on(file.split(".")[0], event.bind(null, client));
       delete require.cache[require.resolve(`./events/${file}`)];
-    } catch (e) { console.warn(e) }
+      client.userLib.sendLog(`[EVENT] "${file}" loaded.`, "START");
+    } catch (e) {
+      console.warn(e);
+      client.userLib.sendLog(`[EVENT] Loading failed "${file}".`, "START");
+    }
   });
 });
+
+client.ws.on('INTERACTION_CREATE', async interaction => {
+  const args = interaction.data["custom_id"].split("_");
+
+  if (!interaction.member) {
+    interaction.member = {};
+    interaction.member.user = interaction.user;
+  }
+
+  if (args[1] !== interaction.member.user.id) return;
+
+  const command = args[0];
+  const cmd = client.commands.get(command);
+  if (!cmd || !cmd.help.interactions) return;
+
+  cmd.interaction(client, interaction, args);
+  client.userLib.sendLog(client.userLib.generateUseLog("interaction", cmd.help.name, interaction), 'Info');
+})
 
 client.commands = new Discord.Collection();
 
@@ -31,6 +53,7 @@ readdir('./commands/', (error, directories) => {
   if (error) return console.warn(error);
   directories.filter(dir => lstatSync(`./commands/${dir}`).isDirectory())
     .forEach(module => {
+      client.userLib.sendLog(`[MODULE] "${module}" module found.`, "START");
       readdir(`./commands/${module}/`, (err, files) => {
         if (err) return console.error(err);
         files.filter(file => file.endsWith('.js'))
@@ -41,7 +64,11 @@ readdir('./commands/', (error, directories) => {
                 props.help.module = module;
                 client.commands.set(file.split(".")[0], props);
               }
-            } catch (e) { console.warn(e) }
+              client.userLib.sendLog(`[COMMAND] "${module}/${file}" loaded.`, "START");
+            } catch (e) {
+              console.warn(e);
+              client.userLib.sendLog(`[COMMAND] Loading failed "${file}".`, "START");
+            }
           });
       });
     });
