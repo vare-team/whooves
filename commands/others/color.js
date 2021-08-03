2047c470e6024f1cb794060616e988841f4de273
Максим <37771198+MasterJ0y@users.noreply.github.com>
2021-08-03 12:28:13 +0300

    /color

--- commands/others/color.js
+++ commands/others/color.js
@@ -1,34 +1,49 @@
 exports.help = {
 	name: 'color',
 	description: 'Ифнормация о цвете',
-	aliases: ['colour'],
 	usage: [{ type: 'text', opt: 0, name: '#HEX' }],
 	dm: 1,
 	tier: 0,
 	cooldown: 5,
 };
 
-exports.run = async (client, msg, args) => {
+exports.command = {
+	name: exports.help.name,
+	description: exports.help.description,
+	options: [
+		{
+			name: 'цвет',
+			description: 'Цвет в шестнадцатиричном формате (#FFFFFF)',
+			type: 3,
+			required: true,
+		},
+	],
+};
+
+exports.run = async (client, interaction) => {
 	let embed = new client.userLib.discord.MessageEmbed();
 
-	if (!/(#|)[0-9A-Fa-f]{6}/g.test(args[0])) {
-		client.userLib.retError(msg, 'Вы указали некорректный цвет!');
+	if (!/(#|)[0-9A-Fa-f]{6}/g.test(interaction.data.options['цвет'].value)) {
+		client.userLib.retError(interaction, 'Вы указали некорректный цвет!');
 		return;
 	}
 
-	let color = parseInt(args[0].replace('#', ''), 16);
+	let color = parseInt(interaction.data.options['цвет'].value.replace('#', ''), 16);
 
 	if (!color || 16777215 < color || 0 > color) {
-		client.userLib.retError(msg, 'Вы указали некорректный цвет!');
+		client.userLib.retError(interaction, 'Вы указали некорректный цвет!');
 		return;
 	}
 
-	let body = await client.userLib.request('https://www.thecolorapi.com/id?hex=' + args[0].replace('#', ''), {
-		json: true,
-	});
+	let body = await client.userLib.request(
+		'https://www.thecolorapi.com/id?hex=' + interaction.data.options['цвет'].value.replace('#', ''),
+		{
+			json: true,
+		}
+	);
 
-	if (!body || !body.name || !body.name.value) return client.userLib.retError(msg, 'Ошибка API.');
-	if (body.code === 400) return client.userLib.retError(msg, 'Ошибка распознования цвета.');
+	if (!body || !body.name || !body.name.value) return client.userLib.retError(interaction, 'Ошибка API.');
+	if (body.code === 400) return client.userLib.retError(interaction, 'Ошибка распознования цвета.');
 
 	embed
 		.setColor(color)
@@ -41,7 +56,7 @@ exports.run = async (client, msg, args) => {
 			`Cyan: **${body.cmyk.c}**\nMagenta: **${body.cmyk.m}**\nYellow: **${body.cmyk.y}**\nBlack: **${body.cmyk.k}**\n`,
 			true
 		)
-		.addField('Util', `${body.rgb.value}\n${body.hsv.value}\n${body.hsl.value}\n${body.cmyk.value}`, true)
-		.setFooter(msg.author.tag, msg.author.displayAvatarURL());
-	msg.channel.send(embed);
+		.addField('Util', `${body.rgb.value}\n${body.hsv.value}\n${body.hsl.value}\n${body.cmyk.value}`, true);
+
+	client.userLib.replyInteraction(interaction, embed);
 };
