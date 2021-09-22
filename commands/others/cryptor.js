f60ada43b9d5f938b08263152e63c118a7004415
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-22 16:19:06 +0300

    Cryptor

--- commands/others/cryptor.js
+++ commands/others/cryptor.js
@@ -1,7 +1,6 @@
 exports.help = {
 	name: 'cryptor',
-	description: 'Простенький шифратор сообщений.\n``crypt - Зашифровать\ndecrypt - Расшифровать``',
-	aliases: ['crypt', 'encrypt', 'ct'],
+	description: 'Простенький шифратор сообщений.',
 	usage: [
 		{ type: 'text', opt: 0, name: 'режим' },
 		{ type: 'text', opt: 0, name: 'текст' },
@@ -11,26 +10,54 @@ exports.help = {
 	cooldown: 1,
 };
 
-exports.run = (client, msg, args) => {
+exports.command = {
+	name: exports.help.name,
+	description: exports.help.description,
+	options: [
+		{
+			name: 'режим',
+			description: 'Режим работы',
+			type: 3,
+			required: true,
+			choices: [
+				{
+					name: 'Зашифровать',
+					value: 'crypt'
+				},
+				{
+					name: 'Дешифровать',
+					value: 'decrypt'
+				}
+			]
+		},
+		{
+			name: 'текст',
+			description: 'Текст',
+			type: 3,
+			required: true,
+		},
+	],
+};
+
+exports.run = (client, interaction) => {
 	let embed = new client.userLib.discord.MessageEmbed()
 		.setColor(client.userLib.colors.inf)
-		.setTitle('🔐 Encryptor')
-		.setFooter(msg.author.tag, msg.author.displayAvatarURL());
+		.setTitle('🔐 Encryptor');
 
-	switch (args[0]) {
+	switch (interaction.options.getString("режим")) {
 		case 'crypt':
-			embed.setDescription('Режим: **шифровка**\n```' + crypt(args.slice(1).join(' ')) + '```');
+			embed.setDescription('Режим: **шифровка**\n```' + crypt(interaction.options.getString("текст")) + '```');
 			break;
 
 		case 'decrypt':
-			embed.setDescription('Режим: **дешифровка**\n```' + decrypt(args.slice(1).join('')) + '```');
+			embed.setDescription('Режим: **дешифровка**\n```' + decrypt(interaction.options.getString("текст")) + '```');
 			break;
 
 		default:
-			return client.userLib.retError(msg, 'Указан неверный режим!');
+			return client.userLib.retError(interaction, 'Указан неверный режим!');
 	}
 
-	msg.channel.send(embed);
+	interaction.reply({embeds: [embed], ephemeral: true});
 };
 
 function crypt(text) {
