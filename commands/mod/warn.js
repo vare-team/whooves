b2f70642ffe8f39577264076d27d7e729e4e9b2b
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-27 19:28:02 +0300

    warn

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -1,14 +1,10 @@
 exports.help = {
 	name: 'warn',
 	description: 'Выдать предупреждение участнику',
-	aliases: ['w'],
 	usage: [
 		{ type: 'user', opt: 0 },
 		{ type: 'text', opt: 1, name: 'причина' },
 	],
-	dm: 0,
-	tier: -1,
-	cooldown: 5,
 };
 
 exports.command = {
@@ -26,33 +22,29 @@ exports.command = {
 			description: 'Причина выдачи предупреждения',
 			type: 3,
 		},
-	],
-	default_permission: false
+	]
 };
 
 exports.run = async (client, interaction) => {
-	if (interaction.options.getString("причина") && interaction.options.getString("причина").length > 300) {
-		client.userLib.retError(interaction, 'Причина не может содержать в себе более 300 символов!');
-		return;
-	}
+	if (interaction.options.getString("причина") && interaction.options.getString("причина").length > 300) return client.userLib.retError(interaction, 'Причина не может содержать в себе более 300 символов!');
 
 	const wUser = interaction.options.getUser("участник"),
-				warn = await client.userLib.promise(client.userLib.db, client.userLib.db.insert,
-					'warns',
-						{
-							userId: wUser.id,
-							guildId: interaction.guildId,
-							who: interaction.user.id,
-							reason: interaction.options.getString("причина"),
-						}
-					),
-				warnInfo = await client.userLib.promise(client.userLib.db, client.userLib.db.queryValue,
-						'SELECT COUNT(*) FROM warns WHERE userId = ? AND guildId = ?',
-						[
-							wUser.id,
-							interaction.guildId
-						]
-					);
+		warn = await client.userLib.promise(client.userLib.db, client.userLib.db.insert,
+			'warns',
+				{
+					userId: wUser.id,
+					guildId: interaction.guildId,
+					who: interaction.user.id,
+					reason: interaction.options.getString("причина"),
+				}
+			),
+		warnInfo = await client.userLib.promise(client.userLib.db, client.userLib.db.queryValue,
+				'SELECT COUNT(*) FROM warns WHERE userId = ? AND guildId = ?',
+				[
+					wUser.id,
+					interaction.guildId
+				]
+			);
 
 	let embed = new client.userLib.discord.MessageEmbed()
 		.setColor(client.userLib.colors.war)
