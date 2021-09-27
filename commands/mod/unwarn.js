9f7c2d532cc8da85421097149ad4ce9ebb0510ce
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-27 19:42:16 +0300

    unwarn

--- commands/mod/unwarn.js
+++ commands/mod/unwarn.js
@@ -1,41 +1,48 @@
 exports.help = {
 	name: 'unwarn',
 	description: 'Снять предупреждение с учасика',
-	aliases: ['delw', 'dw', 'delwarn'],
-	usage: [
-		{ type: 'user', opt: 0 },
-		{ type: 'text', opt: 0, name: 'id' },
-	],
-	dm: 0,
-	tier: -1,
-	cooldown: 15,
 };
 
-exports.run = (client, msg, args) => {
-	client.userLib.db.delete(
+exports.command = {
+	name: exports.help.name,
+	description: exports.help.description,
+	options: [
+		{
+			name: 'участник',
+			description: 'Участник сервера',
+			type: 6,
+			required: true,
+		},
+		{
+			name: 'id',
+			description: 'ID Предупреждения',
+			type: 4,
+			required: true,
+		},
+	],
+};
+
+exports.run = async (client, interaction) => {
+	if (!interaction.options.getUser('участник').id || !interaction.options.getInteger('id')) return;
+
+	const warn = await client.userLib.promise(client.userLib.db, client.userLib.db.delete,
 		'warns',
-		{ warnId: args[1], userId: msg.magicMention.id, guildId: msg.guild.id },
-		(err, affR) => {
-			if (!affR) {
-				client.userLib.retError(msg, 'Тщательно проверив свои записи, я не нашёл предупреждения с такими данными.');
-				return;
-			}
-
-			if (affR > 1) client.userLib.sendLog('Пизда! Удаление варнов сломалось!');
-
-			let embed = new client.userLib.discord.MessageEmbed()
-				.setColor(client.userLib.colors.war)
-				.setTitle(`Снятие предупреждения.`)
-				.setDescription(`Предупреждение *${args[1]}* снято с пользователя ${msg.magicMention}.`)
-				.setTimestamp()
-				.setFooter(msg.author.tag, msg.author.displayAvatarURL());
-
-			msg.channel.send(embed);
-			client.userLib.sendLogChannel('commandUse', msg.guild, {
-				user: { tag: msg.author.tag, id: msg.author.id, avatar: msg.author.displayAvatarURL() },
-				channel: { id: msg.channel.id },
-				content: `снятие предупреждения (ID:${args[1]}) с ${msg.magicMention}`,
-			});
+		{
+			userId: interaction.options.getUser('участник').id,
+			guildId: interaction.guildId,
+			warnId: interaction.options.getInteger('id')
 		}
 	);
+
+	if (!warn.res) return client.userLib.retError(interaction, 'Тщательно проверив свои записи, я не нашёл предупреждения с такими данными.');
+
+	if (warn.res > 1) client.userLib.sendLog('Удаление варнов сломалось!');
+
+	client.userLib.retSuccess(interaction, `С ${interaction.options.getUser('участник')} **снято предупреждение**.`);
+
+	await client.userLib.sendLogChannel('commandUse', interaction.guild, {
+		user: { tag: interaction.user.tag, id: interaction.user.id},
+		channel: { id: interaction.channelId },
+		content: `снятие предупреждения (ID:${interaction.options.getInteger('id')}) с ${interaction.options.getUser('участник').id}`,
+	});
 };
