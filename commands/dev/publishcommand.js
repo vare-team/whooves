01ec96fb4d43f08024ef20fda15e40d5e36d48fa
cipherka <32297717+vlfz@users.noreply.github.com>
2021-09-27 22:01:02 +0500

    Update publishcommand.js

--- commands/dev/publishcommand.js
+++ commands/dev/publishcommand.js
@@ -20,14 +20,19 @@ exports.command = {
 };
 
 exports.run = async (client, interaction) => {
-	const APILinks = {
+	/* const APILinks = {
 		devGuild: 'https://discord.com/api/v9/applications/662302431282987009/guilds/581070953703014401/commands',
 		release: `https://discord.com/api/v9/applications/${client.user.id}/commands/`,
-	};
+	}; */
 
 	let embed = new client.userLib.discord.MessageEmbed().setTimestamp();
+	let req = client.api.applications('662302431282987009').guilds('581070953703014401').commands;
+	// let req = client.api.applications(client.user.id).commands;
+	req.post({ data: client.commands.get(interaction.options.getString('команда')).command }); // returns promise
+	// Если не ошибаюсь, то делается с указанием данных в data
+	// Не сработает - можно попробовать req.post(command)
 
-	client.userLib.request.post(
+	/* client.userLib.request.post(
 		APILinks.devGuild,
 		{
 			headers: { Authorization: 'Bot ' + client.token },
@@ -38,5 +43,7 @@ exports.run = async (client, interaction) => {
 			embed.addField('Error', '```json\n' + JSON.stringify(error, null, ' ') + '```');
 			interaction.reply({ embeds: [embed], ephemeral: true });
 		}
-	);
+	); */
+	embed.addField('Request post', 'success'); // FIXME: Проверка на успешность запроса
+	return interaction.reply({ embeds: [embed], ephemeral: true });
 };
