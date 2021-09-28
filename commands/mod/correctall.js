b85fdf25abeccc90a861f90695e909f8bba02e42
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-28 20:02:44 +0300

    correctall

--- commands/mod/correctall.js
+++ commands/mod/correctall.js
@@ -1,54 +1,49 @@
 exports.help = {
 	name: 'correctall',
 	description: 'Исправляет все никнеймы участников на сервере.',
-	aliases: ['ca'],
-	usage: [],
-	dm: 0,
-	tier: -2,
-	cooldown: 300,
+	extraPermissions: ['MANAGE_NICKNAMES']
 };
 
-exports.run = async (client, msg) => {
-	if (!msg.member.guild.me.hasPermission('MANAGE_NICKNAMES'))
-		return client.userLib.retError(msg, 'Для этой команды необходимо право «Управление никнеймами»!');
+exports.command = {
+	name: exports.help.name,
+	description: exports.help.description,
+};
 
-	const embed = new client.userLib.discord.MessageEmbed()
-		.setColor(client.userLib.colors.inf)
-		.setFooter(msg.author.tag, msg.author.displayAvatarURL())
-		.setTitle(client.userLib.emoji.load + ' Исправление никнеймов...');
+exports.run = async (client, interaction) => {
+	const embed = new client.userLib.discord.MessageEmbed();
 
-	const msgEdit = await msg.channel.send(embed);
+	await interaction.deferReply();
 
-	await msg.guild.members.fetch();
+	await interaction.guild.members.fetch({ force: true });
 	embed.setColor(client.userLib.colors.suc).setDescription('');
 
 	let counter = 0;
 
-	for (let member of msg.guild.members.cache.array()) {
-		const name = member.displayName;
+	for (let member of interaction.guild.members.cache) {
+		const name = member[1].displayName;
 
-		if (member.manageable && !client.userLib.isUsernameCorrect(name) && counter < 25) {
+		if (member[1].manageable && !client.userLib.isUsernameCorrect(name) && counter < 25) {
 			const correctName = client.userLib.getUsernameCorrect(name);
-			await member.edit({ nick: correctName });
+			await member[1].edit({ nick: correctName });
 
 			if (embed.description.length + name.length + correctName.length + 28 < 2000)
 				embed.setDescription(
-					embed.description + `\`\`${counter + 1})\`\` ${name}#${member.user.discriminator} \`\`=>\`\` ${correctName}\n`
+					embed.description + `\`\`${counter + 1})\`\` ${name}#${member[1].user.discriminator} \`\`=>\`\` ${correctName}\n`
 				);
 			else break;
 
 			counter++;
-			await client.userLib.delay(1000);
+			await client.userLib.delay(500);
 		}
 	}
 
 	if (counter) {
 		embed.setDescription(embed.description);
-		embed.setTitle(client.userLib.emoji.ready + ' Отредактировано: ' + counter + '/' + msg.guild.memberCount);
+		embed.setTitle(client.userLib.emoji.ready + ' Отредактировано: ' + counter + '/' + interaction.guild.memberCount);
 	} else {
 		embed.setDescription('');
 		embed.setTitle(client.userLib.emoji.ready + ' Изменений нет!');
 	}
 
-	msgEdit.edit(embed);
+	await interaction.editReply({ embeds: [embed] });
 };
