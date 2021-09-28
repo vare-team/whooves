da8869e32589ac2ef28e231bbf62416c39c86db5
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-28 19:41:39 +0300

    kick

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -1,53 +1,38 @@
 exports.help = {
 	name: 'kick',
 	description: 'Кикнуть участника.',
-	aliases: ['kc'],
-	usage: [
-		{ type: 'user', opt: 0 },
-		{ type: 'text', opt: 1, name: 'причина' },
-		{ type: 'text', opt: 1, name: '-force' },
-	],
-	dm: 0,
-	tier: -1,
-	cooldown: 5,
+	extraPermissions: ['KICK_MEMBERS']
 };
 
-exports.run = async (client, msg, args) => {
-	if (!msg.magicMention.kickable) {
-		client.userLib.retError(msg, 'Я не могу кикнуть этого участника!\nЕго защитная магия превосходит мои умения!');
-		return;
-	}
+exports.command = {
+	name: exports.help.name,
+	description: exports.help.description,
+	options: [
+		{
+			name: 'участник',
+			description: 'Участник сервера',
+			type: 6,
+			required: true,
+		},
+		{
+			name: 'причина',
+			description: 'Причина исключения',
+			type: 3,
+		}
+	]
+};
 
-	let force = args.indexOf('-force');
-	if (force != -1) args.splice(force, 1);
+exports.run = async (client, interaction) => {
+	if (!interaction.options.getMember('участник').kickable) return client.userLib.retError(interaction, 'Я не могу кикнуть этого участника!\nЕго защитная магия превосходит мои умения!');
 
-	if (force != -1 && !client.userLib.checkPerm(-2, { ownerID: msg.guild.ownerID, member: msg.member })) {
-		client.userLib.retError(msg, 'Аргумент ``-force`` доступен только администраторам!');
-		return;
-	}
+	const reason = interaction.options.getString('причина') || 'Причина не указана';
 
-	let warns = await client.userLib.promise(client.userLib.db, client.userLib.db.count, 'warns', {
-		userId: msg.magicMention.id,
-		guildId: msg.guild.id,
-	});
-	warns = warns.res;
+	// await interaction.options.getUser('участник').send(
+	// 	`Вы были кикнуты с сервера \`\`${interaction.guild.name}\`\`, модератором \`\`${interaction.user.tag}\`\`, по причине: ${reason}`
+	// );
+	//TODO: Сделать проверку на возможность отправки сообщения в лс
 
-	if (warns < 3 && force == -1) {
-		client.userLib.retError(msg, 'Для выдачи кика необходимо 3 варнов!\nИспользуй аргумент ``-force`` для кика.');
-		return;
-	}
+	await interaction.options.getMember('участник').kick(interaction.user.tag + ": " + reason);
 
-	let reason = args.slice(1).join(' ') || 'Причина не указана';
-
-	await msg.magicMention.user.send(
-		`Вы были кикнуты с сервера \`\`${msg.guild.name}\`\`, модератором \`\`${msg.author.tag}\`\`, по причине: ${reason}`
-	);
-	msg.magicMention.kick(reason);
-
-	let embed = new client.userLib.discord.MessageEmbed()
-		.setColor(client.userLib.colors.suc)
-		.setDescription(`${msg.magicMention} был кикнут!\nПричина: ${reason}`)
-		.setTimestamp()
-		.setFooter(msg.author.tag, msg.author.displayAvatarURL());
-	msg.channel.send(embed);
+	client.userLib.retSuccess(interaction, `${interaction.options.getMember('участник')} **был кикнут!** ***||*** ${reason}`);
 };
