85d271bc9299caa97f1b934802aab6ff58ab491d
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-10-01 20:01:22 +0300

    kick todo

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -27,10 +27,9 @@ exports.run = async (client, interaction) => {
 
 	const reason = interaction.options.getString('причина') || 'Причина не указана';
 
-	// await interaction.options.getUser('участник').send(
-	// 	`Вы были кикнуты с сервера \`\`${interaction.guild.name}\`\`, модератором \`\`${interaction.user.tag}\`\`, по причине: ${reason}`
-	// );
-	//TODO: Сделать проверку на возможность отправки сообщения в лс
+	await interaction.options.getUser('участник').send(
+		`Вы были кикнуты с сервера \`\`${interaction.guild.name}\`\`, модератором \`\`${interaction.user.tag}\`\`, по причине: ${reason}`
+	).catch(() => client.userLib.sendLog(`${exports.help.name} : DM Send catch! Guild ${interaction.guild.name} (ID:${interaction.guildId}), @${interaction.options.getUser('участник').tag} (ID:${interaction.options.getUser('участник').id})`, 'DM_SEND_ERROR'));
 
 	await interaction.options.getMember('участник').kick(interaction.user.tag + ": " + reason);
 
